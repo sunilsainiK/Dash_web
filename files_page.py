7a0e7125d42392de7b799f87a6cdd8a1acedda62
sunilsainiK <sunilsaini.rocks@gmail.com>
2019-02-13 15:55:51 +0100

    Add files via upload

--- files_page.py
+++ files_page.py
@@ -1,8 +1,17 @@
 import dash
+
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+import base64
+import io
+import pandas as  pd
+import dash_table
+import datetime
+import requests
+import json
 
+from main import app, server
 files_page_layout = html.Div(
                        html.Div([
                          html.Div([html.H1(children='Industrial Event Log Analysis',style={
@@ -13,7 +22,7 @@ files_page_layout = html.Div(
                                                                                    'text-align':'center'                                                                                  }
                       ),
                            html.Div([
-                               html.H2(children='Source Files',style={'padding-left': '2.0%',
+                              html.H2(children='Source Files',style={'padding-left': '2.0%',
                                                                 'padding-right': '2.0%',
                                                                  'margin-top':'2%',
                                                                  'border':'solid',
@@ -24,34 +33,51 @@ files_page_layout = html.Div(
                              html.A(html.Button('Back',id='b-btn',style={'text-align':'left'}),href='/'),
                              html.A(html.Button('Next',id='data-btn',style={'text-align':'left','margin-left':'89.6%'}),href='/'),
 
-                           ]),
-                            html.Div([
+                               ]),html.Hr(),
+
+
+                           html.Div([
                             html.Div([dcc.Dropdown(id='typ-file',options=[{'label':'Csv','value':'csv'},
-                                                                          {'label':'Pickle','value':'pkl'},
-                                                                          {'label':'Json','value':'json'},
-                                                                         {'label':'Excel','value':'xls'}],value='op-li',placeholder='Select file type',
-                                                                         style={'margin-left':'1.5%','margin-top':'2.0%','width':'27.0%'})
+                                                                         {'label':'Pickle','value':'pkl'},
+                                                                         {'label':'Json','value':'json'},
+                                                                        {'label':'Excel','value':'xls'}],value='op-li',placeholder='Select file type',
+                                                                        style={'margin-left':'1.5%','margin-top':'2.0%','width':'27.0%'})
 
-                                               ],),
-                                               html.Div([
-                                               html.Div([dcc.Upload(id='brow-fi',children=html.A(html.Button(['Browse']),),),],style={'margin-left':'45.0%'}),
-                                               html.Div(html.Label('Or',style={'margin-left':'47.5%','margin-top':'2%'}),),
-                                               html.Div([dcc.Upload(id='Drag-Drop',children=html.Div(['Drag & Drop']),),],
-                                                            style={'border':'solid',
-                                                                  'height':'400px',
-                                                                  'margin-left':'1.5%',
-                                                                  'margin-right':'1.5%',
-                                                                  'text-align':'center',
-                                                                  'margin-top':'10%'}),
+                                   ],),
+                                    html.Div([dcc.Upload(id='upload-data',children=html.Div([(html.Button('Upload File'))],style={'margin-left':'45.0%'})),
+                                          html.Hr(),
+                                          html.Hr(),
+                                          html.Hr(),
+                                                dcc.Upload(['Drag and Drop or ',html.A('Select a File')],
+                                                    style={'width': '100%',
+                                                          'height': '60px',
+                                                          'lineHeight': '60px',
+                                                          'borderWidth': '1px',
+                                                          'borderStyle': 'dashed',
+                                                          'borderRadius': '5px',
+                                                          'textAlign': 'center'}),html.Div(id='output-data-upload'),
+                                                          ]),
 
-                                                   ],style={'border':'solid',
-                                                             'height':'800px'}),
-                            html.Div(id='output-image-upload')])
+                               ],style={'border':'solid',
+                                         'height':'600px'}),
 
                          ]),
                  ]),id='files-source-content',
 )
 
-@files_page_layout.callable(Output('output-image-upload', 'children'),
-                           [Input('brow-fi','contents')],
-                           [State('brow-fi','contents')])
+
+@app.callback(Output('output-data-upload', 'children'),
+              [Input('upload-data', 'contents')],
+              [State('upload-data', 'filename'),
+               State('upload-data', 'last_modified')])
+
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_names is not None:
+        global data
+        data=list_of_contents
+        return ([list_of_names,list_of_dates])
+
+@server.route('/data',methods=['GET'])
+def fl_data(data):
+
+  return data
--- main.py
+++ main.py
@@ -1,47 +1,19 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
-from pages import index
+import pandas as pd
+from dash.dependencies import Input, Output, State
+import datetime
+import dash_table
+from flask import Flask, jsonify, abort, make_response, request, url_for
 from pages import files_page
 from pages import db_page
 from pages import Home_Page
+import requests
 
 print(dcc.__version__) # 0.6.0 or above is required
-
-app = dash.Dash()
+server = Flask(__name__)
+app = dash.Dash(__name__, server=server)
 app.css.append_css({'external_url': 'https://cdn.rawgit.com/plotly/dash-app-stylesheets/2d266c578d2a6e8850ebce48fdb52759b2aef506/stylesheet-oil-and-gas.css'})
 
 app.config.suppress_callback_exceptions = True
-
-app.layout = html.Div([
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-])
-
-# Page File_page callback
-@app.callback(dash.dependencies.Output('files-source-content', 'children'),
-              [dash.dependencies.Input('csv-btn', 'value')])
-
-
-# Page 2
-@app.callback(Output('page-2-content', 'children'),
-              [Input('page-2-radios', 'value')])
-
-
-# Index Page callback
-@app.callback(Output('page-content', 'children'),
-              [Input('url', 'pathname')])
-def display_page(pathname):
-    if pathname == '/files_page':
-        return files_page.files_page_layout
-    elif pathname == '/db_page':
-        return db_page.db_page_layout
-    else:
-        return Home_Page.HomePage_layout
-
-
-
-
-if __name__ == '__main__':
-    app.run_server()
